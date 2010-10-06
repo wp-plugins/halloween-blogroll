c6e52fcff1e7efa402baa473b39a8f909a64f4e0
makersonline <makersonline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-06 10:33:52 +0000

    added ghost
    git-svn-id: https://plugins.svn.wordpress.org/halloween-blogroll/trunk@297228 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- anims.js
+++ anims.js
@@ -5,13 +5,46 @@ YUI().use('anim', 'node', function(Y) {
 		if (!container) return;
 
 		var witchNode = Y.Node.create('<div id="hbr_witch" style="position: absolute;"><img src="http://christmas-gifts.makers-online.co.uk/wp-content/plugins/halloween-blogroll/witch.png" /></div>');
+		var ghostNode = Y.Node.create('<div id="hbr_ghost" style="position: absolute; bottom: 20px; right: 0px;"><img src="http://christmas-gifts.makers-online.co.uk/wp-content/plugins/halloween-blogroll/ghost_small.png" /></div>');
 		container.appendChild(witchNode);
+		container.appendChild(ghostNode);
+
+		ghostNode.setStyle('opacity', 0);
+    var ghostAnim = new Y.Anim({
+        node: ghostNode,
+        from: {
+            opacity: 0
+        },
+        to: { 
+            opacity: 1
+        },
+        duration: 10
+    });
+    var ghostAnim2 = new Y.Anim({
+        node: ghostNode,
+        from: {
+            opacity: 1
+        },
+        to: { 
+            opacity: 0
+        },
+        duration: 10
+    });
+		ghostAnim.on('end', function() {
+        ghostAnim2.run();
+		});
+		ghostAnim2.on('end', function() {
+				Y.later(7000, null, function() { ghostAnim.run(); });
+		});
+		ghostAnim.run();
+
 
 		var height = container.getStyle('height');
 		var rHeight = Math.floor(height.replace('px', ''));
 		parseInt(rHeight);
 		Y.log(rHeight);
 		witchNode.setStyle('left', '-100px');
+
 		var newAnim = new Y.Anim({
 				node: witchNode,
 				from: {
@@ -23,8 +56,7 @@ YUI().use('anim', 'node', function(Y) {
 				duration: 10
 		});
 		var randomNumber=Math.floor(Math.random() * rHeight);
-		witchNode.setStyle('top', randomNumber + 'px');
-		
+		witchNode.setStyle('top', randomNumber + 'px');		
 		var repeater = function() {
 				var randomNumber=Math.floor(Math.random() * rHeight);
 				Y.log(randomNumber);
